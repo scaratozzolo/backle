d9a462436bf3b56b9ce66dccb690c48ab7ba1a12
Scott Caratozzolo <scaratozzolo12@gmail.com>
2023-01-27 16:24:02 -0500

    add timezone data to start date

--- backle/environment.py
+++ backle/environment.py
@@ -1,12 +1,19 @@
 import datetime
+from dateutil import tz
 
 
 class BaseEnvironment:
 
     TRADE_AT = 'open'
     FRACTIONAL_SHARES = False
-    START_DATE = datetime.date.today() - datetime.timedelta(weeks=52)
+    START_DATE = datetime.datetime.now(tz=tz.gettz('America/New_York')) - datetime.timedelta(weeks=52)
     END_DATE = None
     COMMISSION_TYPE = "percentage"
     COMMISSION_AMOUNT = "0"
     STARTING_PORTFOLIO_VALUE = 0
+
+
+
+if __name__ == '__main__':
+
+    print(BaseEnvironment.TRADE_AT)

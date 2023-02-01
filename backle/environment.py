dd95ae5a29dee6afd631814e2039e6b0e8f4ebf9
Scott Caratozzolo <scaratozzolo12@gmail.com>
2023-02-01 16:30:49 -0500

    add date_freq to be used for reindexing

--- backle/environment.py
+++ backle/environment.py
@@ -8,6 +8,7 @@ class BaseEnvironment:
     FRACTIONAL_SHARES = True
     START_DATE = pytz.timezone('America/New_York').localize(datetime.datetime(datetime.datetime.today().year-1, datetime.datetime.today().month, datetime.datetime.today().day))
     END_DATE = None
+    DATE_FREQ = "D" # https://pandas.pydata.org/docs/user_guide/timeseries.html#offset-aliases
     COMMISSION_TYPE = "percentage"
     COMMISSION_AMOUNT = 0.0
     STARTING_PORTFOLIO_VALUE = 1
